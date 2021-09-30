e5eeba3525b9d50b81aa3c716f885e4dfa97667c
warrior0986 <diegoguerrero0986@gmail.com>
2021-09-29 19:39:34 -0500

    Setting up search endpoint

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,3 +22,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 Route::get('test', function(){
     return "Test Successful";
 });
+Route::get('/search/{query}', 'HomeController');
